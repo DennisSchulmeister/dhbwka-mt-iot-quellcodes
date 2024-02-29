0b0c0a8c63e074da59d065d2f8da1f24ed4bf21d
Dennis Schulmeister-Zimolong <dennis@patk.org>
2024-02-29 02:52:38 +0100

    ThingsBoard: Alarm-Regeln und Steuer-Widgets im Dashboard

--- 14 Temperatursensor mit ThingsBoard-Backend/iotdevice/main.py
+++ 14 Temperatursensor mit ThingsBoard-Backend/iotdevice/main.py
@@ -9,14 +9,16 @@ import digitalio
 # Kommunikation mit ThingsBoard
 from tb_device_mqtt import TBDeviceMqttClient
 
-enabled = True
-alarm   = False
+enabled   = True
+alarm     = False
+tb_client = None
 
 def receive_shared_attributes(attributes, *args):
     """
     Geteilte Attribute vom Server empfangen.
     """
     shared_attributes = attributes["shared"] if "shared" in attributes else attributes
+    logging.info(f"Empfange geteilte Attribute: {shared_attributes}")
 
     if "enabled" in shared_attributes:
         global enabled
@@ -25,6 +27,29 @@ def receive_shared_attributes(attributes, *args):
     if "alarm" in shared_attributes:
         global alarm
         alarm = shared_attributes["alarm"]
+
+def handle_rpc_request(client, request_body):
+    """
+    RPC (Remote Procedure Call) Anfrage, bei welcher der Server den Raspi auffordert,
+    eine bestimmte Aktion auszuführen, bearbeiten.
+    """
+    logging.info(f"RPC-Anfrage vom Server empfangen: {request_body}")
+
+    # Workaround für fehlende Funktion im Thingsboard-Dashboard. Dort können Control Widgets
+    # wie ein An/Aus-Schalter zwar den Wert eines Shared Attributes anzeigen aber nicht ändern.
+    # Sie können nur einen RPC-Request an das Device schicken. Wir ändern den Wert daher hier
+    # auf dem Device durch einen entsprechende Anfrage an den Server.
+    #
+    # ACHTUNG: Damit das funktioniert, muss die Root Rule Chain in ThingsBoard angepasst werden.
+    # Die hier gesendeten Attribute werden standardmäßig nämlich als "Device Attributes" gespeichert.
+    # Wir müssen sie aber als "Shared Attributes" speichern!
+    if request_body["method"] == "setValue":
+        attribute = request_body["params"]["attribute"]
+        value     = request_body["params"]["value"]
+
+        if attribute:
+            logging.info(f"Setze Attribut {attribute} auf {value}")
+            tb_client.send_attributes({attribute: value})
 
 def main():
     """
@@ -38,6 +63,8 @@ def main():
     
     logging.info(f"Stelle Verbindung zum Backend her: mqtt://{os.environ['TB_HOST']}:{os.environ['TB_PORT']}")
 
+    global tb_client
+
     tb_client = TBDeviceMqttClient(
         host     = os.environ["TB_HOST"],
         port     = int(os.environ["TB_PORT"]),
@@ -47,8 +74,8 @@ def main():
     tb_client.connect()
 
     tb_client.request_attributes(shared_keys=["enabled", "alarm"], callback=receive_shared_attributes)
-    tb_client.subscribe_to_attribute("enabled", callback=receive_shared_attributes)
-    tb_client.subscribe_to_attribute("alarm", callback=receive_shared_attributes)
+    tb_client.subscribe_to_all_attributes(receive_shared_attributes)
+    tb_client.set_server_side_rpc_request_handler(handle_rpc_request)
 
     # Objekte für die Hardwarezugriffe erzeugen
     logging.info("Initialisiere Hardwarebausteine")
